8b1b54050abbdb809b757c54ea1c7ef0d7776c17
EmeraldMike <EmeraldMike360@gmail.com>
2022-02-11 17:57:21 -0500

    animation updates

--- src/modules/animations.js
+++ src/modules/animations.js
@@ -1,27 +1,25 @@
 var color = require('./color.js');
 var cursor = require('./cursor.js');
 var tools = require('./tools.js');
-exports.loading = async function (time, text) {
+exports.loading = async function (condition, text) {
 var txt = text || "";
 cursor.hide();
-    for (time > 0; time--;) {
+    while (condition) {
         console.log(color.colorize('cyan', "↻ " + txt));
         cursor.move_up(1)
-        await tools.sleep(10);
+        await tools.sleep(100);
         console.log(color.colorize('cyan', "⟳ " + txt));
         cursor.move_up(1)
-        await tools.sleep(10);
+        await tools.sleep(100);
         console.log(color.colorize('cyan', "⤸ " + txt));
         cursor.move_up(1)
-        await tools.sleep(10);
+        await tools.sleep(100);
         console.log(color.colorize('cyan', "⤾ " + txt));
         cursor.move_up(1)
-        await tools.sleep(10);
+        await tools.sleep(100);
         console.log(color.colorize('cyan', "↻ " + txt));
         cursor.move_up(1)
-        await tools.sleep(10);
     }
     console.log(color.colorize('cyan', "◯  " + txt));
     cursor.show();
-    await time < 1;
 }
--- tests/test.js
+++ tests/test.js
@@ -1,9 +1,9 @@
 var catcl = require('../src/main.js');
 
+catcl.loading(true,'Loading');
 catcl.log('Log');
 catcl.success('Success');
 catcl.warn('Warn');
 catcl.info('Info');
 catcl.error('Error');
-catcl.loading(4,'Loading');
 catcl.log('Loading Done');
